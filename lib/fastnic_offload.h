62ee387cfdb41e4a5579f07c17c9cc23abd46a7d
qynZJU <qyning710@zju.edu.cn>
2023-09-03 15:08:49 +0800

    off2 update

--- lib/fastnic_offload.h
+++ lib/fastnic_offload.h
@@ -4,7 +4,7 @@
 #include "flow.h"
 
 #define FASTNIC_OFFLOAD 1 //only support tcp & udp now
-#define FASTNIC_OFF_IGNO 1 //ignore emc, smc, cmap
+#define OFFLOAD_META 1 //define meta_data for offloading and read it from packets
 
 #define OFFLOAD_THRE 10 
 
